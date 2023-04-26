8bff36dcce1ee6860756b45e1bf634a9f6336e58
nerbrain <malikmox@gmail.com>
2023-04-26 20:30:46 +0000

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec{ 'install':
   provider => shell,
-  command  => 'sudo apt-get -y updates ; sudo apt-get -y nginx ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/google.com permanent;/" /etc/nginx/sites-available/default ; service nginx start ; echo "Hello World!" > /var/www/html/index.nginx-debian.html',
+  command  => 'sudo apt-get -y updates ; sudo apt-get -y nginx ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/google.com permanent;/" /etc/nginx/sites-available/default ; service nginx start ; echo "Hello World!" > /var/www/html/index.nginx-debian.html ; sudo service nginx restart',
 }
