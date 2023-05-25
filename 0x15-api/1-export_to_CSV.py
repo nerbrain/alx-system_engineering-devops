9b0d2a16240781b58b8e40f793c6885add789116
nerbrain <malikmox@gmail.com>
2023-05-25 14:09:39 +0000

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Employee Info """
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
+
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    list_task = []
+    for task in tasks:
+        list_task.append(
+            [userid, name, task.get('completed'), task.get('title')])
+
+    filename = '{}.csv'.format(userid)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in list_task:
+            employee_writer.writerow(task)
