614dfa134aae8a0ce79b3ee4b1b899425948e348
nerbrain <malikmox@gmail.com>
2023-06-07 20:10:23 +0000

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
-# Wordpress 500 Error
+# Fixing Wordpress 500 Error
 
 exec {'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
   onlyif   => 'cat /var/www/html/wp-settings.php | grep class-wp-locale.phpp'
 }
+
