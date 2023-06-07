b4a14a23b5a6dc6eaf6b90568e5f2e819e3d7c0d
nerbrain <malikmox@gmail.com>
2023-06-07 20:06:38 +0000

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Wordpress 500 Error
+
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  onlyif   => 'cat /var/www/html/wp-settings.php | grep class-wp-locale.phpp'
+}
