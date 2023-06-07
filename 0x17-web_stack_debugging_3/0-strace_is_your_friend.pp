c2a5fb5da44bacfeae1d5381d28f2998d19496c0
nerbrain <malikmox@gmail.com>
2023-06-07 20:13:09 +0000

    fix2

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,6 +3,5 @@
 exec {'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-  onlyif   => 'cat /var/www/html/wp-settings.php | grep class-wp-locale.phpp'
 }
 
