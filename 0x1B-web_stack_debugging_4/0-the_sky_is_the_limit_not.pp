45fc7639b412cc4d731beb0ab384f8cddfd4dc92
nerbrain <malikmox@gmail.com>
2023-06-14 19:39:45 +0000

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# remove nginx limit
+exec {'remove limit':
+    command => 'mv /etc/default/nginx /etc/default/nginx.bak; sudo service nginx restart',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    onlyif  => 'find /etc/default/nginx'
+}
+
